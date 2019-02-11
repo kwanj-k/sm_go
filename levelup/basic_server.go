03e2c456a43090c3f402e1a9198bd31e736ed950
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-02-11 09:17:29 +0300

    levelup
    * simple todo example
    
    * phonebook

---
+++ levelup/basic_server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	Name string `json:"name"`
+	Completed bool `json:"completed"`
+	Due time.Time `json:"due"`
+}
+
+type Todos []Todo
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todod/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+	json.NewEncoder(w).Encode(todos)
+
+}
+ 
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
+}
